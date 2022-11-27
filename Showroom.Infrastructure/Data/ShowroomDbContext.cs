0139c301e5d71ee6d0168e6fa8bc447755f0d9ff
Borislav Yurkev <borislavurkev@gmail.com>
2022-11-27 11:31:26 +0200

    add forget code

--- Showroom.Infrastructure/Data/ShowroomDbContext.cs
+++ Showroom.Infrastructure/Data/ShowroomDbContext.cs
@@ -21,8 +21,11 @@ namespace Showroom.Infrastructure.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyConfiguration(new InitialDataConfiguration<User>(@"../Showroom.Infrastructure/InitialSeed/users.json"));
+            modelBuilder.ApplyConfiguration(new InitialDataConfiguration<Car>(@"../Showroom.Infrastructure/InitialSeed/cars.json"));
             modelBuilder.ApplyConfiguration(new InitialDataConfiguration<Garage>(@"../Showroom.Infrastructure/InitialSeed/garages.json"));
+            modelBuilder.ApplyConfiguration(new InitialDataConfiguration<Part>(@"../Showroom.Infrastructure/InitialSeed/parts.json"));
+            modelBuilder.ApplyConfiguration(new InitialDataConfiguration<Entities.Showroom>(@"../Showroom.Infrastructure/InitialSeed/showrooms.json"));
+            modelBuilder.ApplyConfiguration(new InitialDataConfiguration<User>(@"../Showroom.Infrastructure/InitialSeed/users.json"));
         }
     }
 }
